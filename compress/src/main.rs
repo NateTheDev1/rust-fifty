38e963b6b0cc4665a4172c3af409d1b0114d4af3
Nathaniel Richards <nthnlrichards@gmail.com>
2022-12-08 12:04:36 -0500

    Created a file compressor

---
+++ compress/src/main.rs
@@ -0,0 +1,36 @@
+extern crate flate2;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::env::args;
+use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
+use std::time::Instant;
+
+fn main() {
+    if args().len() != 3 {
+        eprint!("Usage: `source` `target`");
+        return;
+    }
+
+    let file_ref = File::open(args().nth(1).unwrap()).unwrap();
+
+    let mut input = BufReader::new(file_ref);
+    let mut output = File::create(args().nth(2).unwrap()).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let start = Instant::now();
+
+    copy(&mut input, &mut encoder).unwrap();
+
+    let output = encoder.finish().unwrap();
+
+    println!(
+        "Source len: {:?}",
+        input.get_ref().metadata().unwrap().len()
+    );
+
+    println!("Target Len: {:?}", output.metadata().unwrap().len());
+
+    println!("Elapsed: {:?}", start.elapsed());
+}
